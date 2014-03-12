cc30ff8dfdfca5d2ef4e00e1dd6c36462d2d82d9
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-03-12 02:23:04 -0800

    added a small tool to convert read files from any of our supported formats to plain text - also useful as an example
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18040166]

---
+++ nvExtractReads/nvExtractReads.cu
@@ -0,0 +1,137 @@
+/*
+ * nvbio
+ * Copyright (C) 2011-2014, NVIDIA Corporation
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+// nvExtractReads.cu
+//
+
+#include <nvbio/basic/timer.h>
+#include <nvbio/basic/shared_pointer.h>
+#include <nvbio/io/reads/reads.h>
+#include <nvbio/fmindex/dna.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+
+using namespace nvbio;
+
+bool read(const char* reads_name, FILE* output_file, const io::QualityEncoding qencoding, const io::ReadEncoding flags)
+{
+    log_visible(stderr, "opening read file \"%s\"\n", reads_name);
+    SharedPointer<nvbio::io::ReadDataStream> read_data_file(
+        nvbio::io::open_read_file(reads_name,
+        qencoding,
+        uint32(-1),
+        uint32(-1),
+        flags )
+    );
+
+    if (read_data_file == NULL || read_data_file->is_ok() == false)
+    {
+        log_error(stderr, "    failed opening file \"%s\"\n", reads_name);
+        return false;
+    }
+
+    const uint32 batch_size = 512*1024;
+
+    std::vector<char> char_read( 1024*1024 );
+
+    uint32 n_reads = 0;
+
+    // loop through all read batches
+    while (1)
+    {
+        // load a new batch of reads
+        SharedPointer<io::ReadData> h_read_data( read_data_file->next( batch_size ) );
+        if (h_read_data == NULL)
+            break;
+
+        // loop through all reads
+        for (uint32 i = 0; i < h_read_data->size(); ++i)
+        {
+            const io::ReadData::read_string read = h_read_data->get_read(i);
+
+            dna_to_string( read, read.length(), &char_read[0] );
+
+            char_read[ read.length() ] = '\n';
+
+            fwrite( &char_read[0], sizeof(char), read.length()+1, output_file );
+        }
+
+        n_reads += h_read_data->size();
+
+        log_verbose(stderr,"\r    %u reads    ", n_reads);
+    }
+    log_verbose_cont(stderr,"\n");
+    return true;
+}
+
+int main(int argc, char* argv[])
+{
+    const char* reads_name  = argv[argc-2];
+    const char* out_name    = argv[argc-1];
+    bool  forward           = true;
+    bool  reverse           = true;
+    io::QualityEncoding qencoding = io::Phred33;
+
+    for (int i = 0; i < argc - 2; ++i)
+    {
+        if (strcmp( argv[i], "-verbosity" ) == 0 ||
+                 strcmp( argv[i], "--verbosity" ) == 0)
+        {
+            set_verbosity( Verbosity( atoi( argv[++i] ) ) );
+        }
+        else if (strcmp( argv[i], "-F" ) == 0)  // skip forward strand
+        {
+            forward = false;
+        }
+        else if (strcmp( argv[i], "-R" ) == 0)  // skip reverse strand
+        {
+            reverse = false;
+        }
+    }
+
+    FILE* output_file = fopen( out_name, "w" );
+    if (output_file == NULL)
+    {
+        log_error(stderr, "    failed opening file \"%s\"\n", out_name);
+        return 1;
+    }
+
+    log_visible(stderr,"nvExtractReads... started\n");
+
+    // NOTE: at the moment the forward and reverse strands are not interleaved: we place
+    // first all the forward and then all the reverse strands - might want to fix this.
+    if (forward)
+    {
+        if (read( reads_name, output_file, qencoding, io::ReadEncoding(0) ) == false)
+            return 1;
+    }
+    if (reverse)
+    {
+        if (read( reads_name, output_file, qencoding, io::ReadEncoding(io::REVERSE | io::COMPLEMENT) ) == false)
+            return 1;
+    }
+
+    fclose( output_file );
+
+    log_visible(stderr,"nvExtractReads... done\n");
+    return 0;
+}
