0f568980245eb93ff087b84506ba4c18b4b3b4f2
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-03-17 02:46:17 -0800

    remove unused var
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18073142]

--- nvBowtie/bowtie2/cuda/bowtie2_cuda_driver.cu
+++ nvBowtie/bowtie2/cuda/bowtie2_cuda_driver.cu
@@ -180,8 +180,6 @@ int driver(
 
     // compute band length
     const uint32 band_len = Aligner::band_length( params.max_dist );
-
-    const uint32 genome_length = driver_data_host.genome_length();
     
     // print command line options
     log_visible(stderr, "  mode           = %s\n", mapping_mode( params.mode ));
@@ -533,8 +531,6 @@ int driver(
 
     // compute band length
     const uint32 band_len = Aligner::band_length( params.max_dist );
-
-    const uint32 genome_length = driver_data_host.genome_length();
 
     // print command line options
     log_visible(stderr, "  mode           = %s\n", mapping_mode( params.mode ));
