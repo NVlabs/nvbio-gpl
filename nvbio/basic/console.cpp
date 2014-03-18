fbf440ef0ace36c642d684f1559235781928ca57
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-03-18 00:28:52 -0800

    fixed linux logging
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18081946]

--- nvbio/basic/console.cpp
+++ nvbio/basic/console.cpp
@@ -17,31 +17,7 @@
  */
 
 #include <nvbio/basic/console.h>
-
-#if WIN32
-#include <nvbio/basic/threads.h>
-#include <windows.h>
 #include <string>
-
-unsigned int TEXT_BLUE   = FOREGROUND_BLUE;
-unsigned int TEXT_RED    = FOREGROUND_RED;
-unsigned int TEXT_GREEN  = FOREGROUND_GREEN;
-unsigned int TEXT_BRIGHT = FOREGROUND_INTENSITY;
-
-namespace { nvbio::Mutex s_mutex; }
-
-Verbosity s_verbosity = V_VERBOSE;
-
-void set_verbosity(Verbosity level)
-{
-    s_verbosity = level;
-}
-
-static void textcolor(unsigned int color)
-{
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);  // Get handle to standard output
-    SetConsoleTextAttribute(hConsole,color);  // set the text attribute of the previous handle
-}
 
 std::string retokenize(const char* format, const char* prefix)
 {
@@ -81,6 +57,30 @@ std::string retokenize(const char* format, const char* prefix)
             new_format.append( 1u,*p );
     }
     return new_format;
+}
+
+#if WIN32
+#include <nvbio/basic/threads.h>
+#include <windows.h>
+
+unsigned int TEXT_BLUE   = FOREGROUND_BLUE;
+unsigned int TEXT_RED    = FOREGROUND_RED;
+unsigned int TEXT_GREEN  = FOREGROUND_GREEN;
+unsigned int TEXT_BRIGHT = FOREGROUND_INTENSITY;
+
+namespace { nvbio::Mutex s_mutex; }
+
+Verbosity s_verbosity = V_VERBOSE;
+
+void set_verbosity(Verbosity level)
+{
+    s_verbosity = level;
+}
+
+static void textcolor(unsigned int color)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);  // Get handle to standard output
+    SetConsoleTextAttribute(hConsole,color);  // set the text attribute of the previous handle
 }
 
 void log_visible(FILE* stream, const char* format, ...)
@@ -310,8 +310,9 @@ void log_visible(FILE* stream, const char* format, ...)
 {
     if (s_verbosity >= V_VISIBLE)
     {
+        const std::string new_format = retokenize( format, "visible : " );
         char col_format[2048];
-        sprintf( col_format, "%svisible : %s", TEXT_BRIGHT, format );
+        sprintf( col_format, "%s%s", TEXT_BRIGHT, new_format.c_str() );
         va_list args;
         va_start(args, format);
         vfprintf(stream, col_format, args);
@@ -322,8 +323,9 @@ void log_info(FILE* stream, const char* format, ...)
 {
     if (s_verbosity >= V_INFO)
     {
+        const std::string new_format = retokenize( format, "info    : " );
         char col_format[2048];
-        sprintf( col_format, "%sinfo    : %s", TEXT_NORMAL, format );
+        sprintf( col_format, "%s%s", TEXT_NORMAL, new_format.c_str() );
         va_list args;
         va_start(args, format);
         vfprintf(stream, col_format, args);
@@ -334,8 +336,9 @@ void log_stats(FILE* stream, const char* format, ...)
 {
     if (s_verbosity >= V_STATS)
     {
+        const std::string new_format = retokenize( format, "stats   : " );
         char col_format[2048];
-        sprintf( col_format, "%sstats   : %s", TEXT_BLUE, format );
+        sprintf( col_format, "%s%s", TEXT_BLUE, new_format.c_str() );
         va_list args;
         va_start(args, format);
         vfprintf(stream, col_format, args);
@@ -346,8 +349,9 @@ void log_verbose(FILE* stream, const char* format, ...)
 {
     if (s_verbosity >= V_VERBOSE)
     {
+        const std::string new_format = retokenize( format, "verbose : " );
         char col_format[2048];
-        sprintf( col_format, "%sverbose : %s", TEXT_GREEN, format );
+        sprintf( col_format, "%s%s", TEXT_GREEN, new_format.c_str() );
         va_list args;
         va_start(args, format);
         vfprintf(stream, col_format, args);
@@ -358,8 +362,9 @@ void log_debug(FILE* stream, const char* format, ...)
 {
     if (s_verbosity >= V_DEBUG)
     {
+        const std::string new_format = retokenize( format, "debug   : " );
         char col_format[2048];
-        sprintf( col_format, "%sdebug    : %s", TEXT_RED, format );
+        sprintf( col_format, "%s%s", TEXT_RED, new_format.c_str() );
         va_list args;
         va_start(args, format);
         vfprintf(stream, col_format, args);
@@ -370,8 +375,9 @@ void log_warning(FILE* stream, const char* format, ...)
 {
     if (s_verbosity >= V_ERROR)
     {
+        const std::string new_format = retokenize( format, "warning : " );
         char col_format[2048];
-        sprintf( col_format, "%swarning : %s", TEXT_CYAN, format );
+        sprintf( col_format, "%s%s", TEXT_CYAN, new_format.c_str() );
         va_list args;
         va_start(args, format);
         vfprintf(stream, col_format, args);
@@ -382,8 +388,9 @@ void log_error(FILE* stream, const char* format, ...)
 {
     if (s_verbosity >= V_ERROR)
     {
+        const std::string new_format = retokenize( format, "error   : " );
         char col_format[2048];
-        sprintf( col_format, "%serror   : %s", TEXT_BRIGHT_RED, format );
+        sprintf( col_format, "%s%s", TEXT_BRIGHT_RED, new_format.c_str() );
         va_list args;
         va_start(args, format);
         vfprintf(stream, col_format, args);
