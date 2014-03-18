dcaf067b791360be7aaf7ecd9a9feb9a3e1aee04
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-03-18 00:10:41 -0800

    updated to latest version of moderngpu
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18081766]

--- nvbio/sufsort/compression_sort.h
+++ nvbio/sufsort/compression_sort.h
@@ -350,8 +350,8 @@ void CompressionSort::sort(
         mgpu::SegSortPairsFromFlags(
             nvbio::device_view( d_keys ),
             nvbio::device_view( d_indices ),
-            d_comp_flags,
             n_active_suffixes,
+            d_comp_flags,
             *m_mgpu );
 
         NVBIO_CUDA_DEBUG_STATEMENT( cudaDeviceSynchronize() );
@@ -629,8 +629,8 @@ void CompressionSort::sort(
                 mgpu::SegSortPairsFromFlags(
                     nvbio::device_view( d_keys ),
                     nvbio::device_view( d_indices ),
-                    d_comp_flags,
                     n_active_strings,
+                    d_comp_flags,
                     *m_mgpu );
 
                 NVBIO_CUDA_DEBUG_STATEMENT( cudaDeviceSynchronize() );
