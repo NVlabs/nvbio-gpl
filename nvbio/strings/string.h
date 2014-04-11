7f927c3bc1d508abf430e0563842451ce297562f
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-04-11 02:48:53 -0800

    added string.h
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18253815]

---
+++ nvbio/strings/string.h
@@ -0,0 +1,37 @@
+/*
+ * nvbio
+ * Copyright (C) 2011-2014, NVIDIA Corporation
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#pragma once
+
+#include <nvbio/basic/types.h>
+#include <nvbio/basic/vector_wrapper.h>
+
+
+namespace nvbio {
+
+///@addtogroup Strings
+///@{
+
+/// length of an ASCII string
+///
+NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+uint64 length(const char* str) { return strlen( str ); }
+
+///@} Strings
+
+} // namespace nvbio
