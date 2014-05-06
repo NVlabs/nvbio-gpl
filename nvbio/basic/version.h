01b324bfe69bcb686ea15b55bd6ba8d83bf77001
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-05-06 09:41:38 -0800

    - bumped up the version number to 0.9.6 - added an NVBIO_VERSION define
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18429269]

---
+++ nvbio/basic/version.h
@@ -0,0 +1,23 @@
+/*
+ * nvbio
+ * Copyright (C) 2011-2014, NVIDIA Corporation
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#pragma once
+
+#define NVBIO_VERSION           90600                       // 0.9.6
+#define NVBIO_MAJOR_VERSION     (NVBIO_VERSION / 1000)
+#define NVBIO_MINOR_VERSION     (NVBIO_VERSION % 1000)
