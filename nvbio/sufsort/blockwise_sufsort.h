55e1ca224c5a59d60e2093fb02774acf0cb23693
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-07-15 06:33:49 -0800

    added assertion
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18694481]

--- nvbio/sufsort/blockwise_sufsort.h
+++ nvbio/sufsort/blockwise_sufsort.h
@@ -470,6 +470,8 @@ void blockwise_build(
             d_sample.begin() + sample_size,
             in_dcs,
             d_temp_storage );
+
+        assert( sample_size <= estimated_sample_size );
     }
 
     // alloc enough space for the DC ranks
